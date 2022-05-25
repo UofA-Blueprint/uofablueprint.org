d9ea7fa23c4a99b62f502eef916e8d00103a36c0
Filippo Ciandy <filippo.ciandy10@gmail.com>
2022-05-25 13:15:57 -0600

    Moved themes to config and setup themeprovider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,21 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { createTheme } from '@mui/material/styles';
+
 import { Header, Footer } from './components/shared'
+import { Theme } from './config/Theme'
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 
-
-/* eslint-disable no-unused-vars */
-var theme = createTheme({
-    palette: {
-        primary: {
-            light: '#FFFFFF', // White
-            main: '#147BE5', // Primary color
-            dark: '#0B2538', // Dark blue
-            contrastText: '#000',
-        },
-        secondary: {
-            light: '#F8F8F8', //Light grey
-            main: '#AFAFAF', // Grey
-            dark: '#333333', //Dark grey
-            contrastText: '#000',
-        },
-    },
-});
-/* eslint-enable no-unused-vars */
 
 
 function App() {
     return (
-        <div>
+        <ThemeProvider theme={Theme}>
+            <CssBaseline enableColorScheme />
             <Header />
             <Outlet />
             <Footer />
-        </div>
+        </ThemeProvider>
     )
 }
 
