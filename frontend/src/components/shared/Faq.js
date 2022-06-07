ab6b1de6ce1d441a389894dff77041b72a0045a6
Filippo Ciandy <filippo.ciandy10@gmail.com>
2022-06-06 20:14:43 -0600

    Doing FAQ section

--- frontend/src/components/shared/Faq.js
+++ frontend/src/components/shared/Faq.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material';
 
-function Faq() {
-    return <div>-faq process component placeholder-</div>
+
+function Faq(props) {
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }} bgcolor="primary.dark">
+            <Typography fontSize={20} color='primary.light'> Frequently Asked Questions</Typography>
+            <h1> hello, {props.name} </h1>
+        </Box>
+    )
 }
 
 export default Faq
--- frontend/src/pages/StudentApply.js
+++ frontend/src/pages/StudentApply.js
@@ -9,7 +9,7 @@ function StudentApply() {
             <Banner />
             <ApplyProcess />
             <Criteria />
-            <Faq />
+            <Faq name="fil" />
         </div>
     )
 }
