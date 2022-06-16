0d40404f6bfde123cc7de2653f60e66eed735ac1
Gurveer Sohal <gurveersohal433@gmail.com>
2022-06-16 11:21:08 -0600

    styled mission statement component

--- frontend/src/components/about/Mission.js
+++ frontend/src/components/about/Mission.js
@@ -1,7 +1,29 @@
+import { Typography } from '@mui/material'
 import React from 'react'
+import { Box } from '@mui/system';
 
 function Mission() {
-    return <div>-mission component placeholder-</div>
+    return (
+        <Box sx={{
+            background: '#f6f6f6',
+            textAlign: 'center'
+        }}>
+            <Typography variant='h3' color='primary' sx={{
+                p: 4,
+                fontWeight: 'medium'
+            }}>
+                Our Mission Statement
+            </Typography>
+
+            <Typography variant='body1' sx={{
+                p: 4,
+                fontSize: 22
+            }}>
+                To make beautiful engineering accessible and useful for those who create
+                communities and promote public welfare.
+            </Typography>
+        </Box>
+    )
 }
 
 export default Mission
