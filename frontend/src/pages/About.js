18ea6e202559ef4b92eb1c977290c611a8fecfe8
Gurveer Sohal <gurveersohal433@gmail.com>
2022-06-20 11:40:38 -0600

    placing items in a grid

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -2,15 +2,29 @@ import React from 'react'
 
 import { Banner } from '../components/shared'
 import { Member, Mission, Value } from '../components/about'
+import { Grid } from '@mui/material'
 
 function About() {
     return (
-        <div>
-            <Banner />
-            <Mission />
-            <Value />
-            <Member />
-        </div>
+        <Grid container>
+
+            <Grid item xs={12}>
+                <Banner />
+            </Grid>
+
+            <Grid item xs={12}>
+                <Mission />
+            </Grid>
+            
+            <Grid item xs={12}>
+                <Value />
+            </Grid>
+                 
+            <Grid item xs={12}>
+                <Member />
+            </Grid>
+
+        </Grid>
     )
 }
 
