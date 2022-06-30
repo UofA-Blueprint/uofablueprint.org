966fd8cf6833ee00541d6dab39932d092ca628e9
Filippo Ciandy <filippo.ciandy10@gmail.com>
2022-06-30 01:06:49 -0600

    Fixed tests

--- frontend/src/tests/components/shared/Footer.test.js
+++ frontend/src/tests/components/shared/Footer.test.js
@@ -4,5 +4,5 @@ import { render, screen } from '@testing-library/react'
 test('renders footer', () => {
     render(<Footer />)
     expect(screen.findAllByLabelText("Blueprint 2022"))
-    expect(screen.getByRole("footer")).toHaveTextContent("Blueprint 2022 General Home Team Projects For Students Apply Process FAQs For Nonprofits Apply Process FAQs Contact Us Email LinkedIn Instagram Github")
+    expect(screen.getByRole("footer")).toHaveTextContent("Blueprint 2022 General Home General Projects For Students About FAQ For Nonprofits About FAQ Contact Us Email LinkedIn Instagram Github")
 })
