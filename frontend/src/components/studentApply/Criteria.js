c102b88b4049e48307d828677408a85f8a3f9f84
Juan Fernandez <jfernan123@ualberta.ca>
2022-07-03 13:27:54 -0600

    Grid now rescales based on window size

--- frontend/src/components/studentApply/Criteria.js
+++ frontend/src/components/studentApply/Criteria.js
@@ -3,39 +3,34 @@ import studentCriteria from '../../../src/data/studentCriteria.json'
 import {Grid, Typography} from '@mui/material'
 function Criteria() {
     return (
-        //First I will render each image
-
-        //Then I will render each title
-
-        //Then I will render each text box
-
-        //Then I will put them side by side in a grid with 3 textboxes as columns
-
-        <div>
-            <Grid space container ={3} alignItems={"center"} justifyItems="center">
-                {studentCriteria.map(criteria => {
-                    return( <Grid item xs = {4}
+        <div overflow = "hidden">
+            <Grid container spacing ={2} align="Center" >
+                {studentCriteria.map(criteria => { //Maps the criteria json to a grid with title and images
+                    return( 
+                    <Grid item xs = {12} sm = {6} lg = {4} 
+                    marginBottom = {4} marginTop ={2} overflow = "hidden"//Resizes the grid based on screen size
                      key = {criteria.title}
-                     >
+                     > 
                         <img src = {criteria.image} width = {100} height = {100}/>
-                        <Typography 
-                        component = {"h1"}
+                        <Typography  //Sets h1 for the titles and h2 for the text contents
+                        component = {"h1"} 
                         fontSize = {"24px"} 
                         fontWeight ={"bold"}>
 
                             {criteria.title}
-                            <Typography 
-                            component = {"h2"}
-                            fontFamily={"Roboto"} 
-                            fontSize = {"24px"} 
-                            fontWeight= {400} 
-                            color={"#0B2538"}
-                            width = {310}
-                            alignItems={"right"}
-                            >
 
-                                {criteria.body}
-                            </Typography>
+                        </Typography>
+                        
+                        <Typography 
+                        component = {"h2"}
+                        fontFamily={"Roboto"} 
+                        fontSize = {"24px"}  
+                        fontWeight= {400} 
+                        color={"#0B2538"}
+                        width = {310}
+                        >
+                            {criteria.body}
+
                         </Typography>
                     </Grid>
                     )
