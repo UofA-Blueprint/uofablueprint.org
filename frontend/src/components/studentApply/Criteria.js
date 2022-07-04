3abffe13bca96fe04c83f8b598d6177edc01ecaf
Juan Fernandez <jfernan123@ualberta.ca>
2022-07-03 18:18:15 -0600

    Made it look exactly like the figma

--- frontend/src/components/studentApply/Criteria.js
+++ frontend/src/components/studentApply/Criteria.js
@@ -1,37 +1,45 @@
 import React from 'react'
 import studentCriteria from '../../../src/data/studentCriteria.json'
-import {Grid, Typography} from '@mui/material'
+import {Grid, Typography, Box} from '@mui/material'
 function Criteria() {
     return (
         <div overflow = "hidden">
-            <Grid container spacing ={2} align="Center" >
+            <Grid container spacing ={1} align="Center" justify="space-between" >
                 {studentCriteria.map(criteria => { //Maps the criteria json to a grid with title and images
                     return( 
-                    <Grid item xs = {12} sm = {6} lg = {4} 
-                    marginBottom = {4} marginTop ={2} overflow = "hidden"//Resizes the grid based on screen size
+                    
+                    <Grid item xs = {12} sm = {6} lg = {4}
+                     marginBottom = {"4%"}
+                     marginTop ={"2%"}
+                     overflow = "hidden"//Resizes the grid based on screen size
                      key = {criteria.title}
                      > 
-                        <img src = {criteria.image} width = {100} height = {100}/>
-                        <Typography  //Sets h1 for the titles and h2 for the text contents
+
+                    <Box width = "75%">
+                        <div align={"left"}>
+                        <img src = {criteria.image} width = {"30%"} height = {"30%"} marginBottom = {"10%"}/>
+
+                        <Typography  //Sets h1 for he titles and h2 for the text contents
                         component = {"h1"} 
                         fontSize = {"24px"} 
-                        fontWeight ={"bold"}>
-
+                        fontWeight ={"bold"}
+                        marginLeft = {"2%"}
+                        marginTop = {"5%"}>
                             {criteria.title}
-
                         </Typography>
-                        
+                        </div>
                         <Typography 
                         component = {"h2"}
                         fontFamily={"Roboto"} 
                         fontSize = {"24px"}  
                         fontWeight= {400} 
+                        align = {"Left"}
                         color={"#0B2538"}
-                        width = {310}
                         >
                             {criteria.body}
 
                         </Typography>
+                        </Box>
                     </Grid>
                     )
                 })}
