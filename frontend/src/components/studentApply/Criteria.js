a5896ba25f5d92f74639e566fed9290473bd26e1
Juan Fernandez <jfernan123@ualberta.ca>
2022-07-10 13:46:01 -0600

    Implemented the fixes

--- frontend/src/components/studentApply/Criteria.js
+++ frontend/src/components/studentApply/Criteria.js
@@ -4,37 +4,43 @@ import {Grid, Typography, Box} from '@mui/material'
 function Criteria() {
     return (
         <div overflow = "hidden">
+            <Typography
+            align = {"left"}
+            component = {"h1"} 
+            fontSize = {"48px"} 
+            fontWeight ={"bold"}
+            marginLeft = {"3.5%"}
+            marginBottom = {"3%"}>
+                Decision Criteria
+            </Typography>
             <Grid container spacing ={1} align="Center" justify="space-between" >
                 {studentCriteria.map(criteria => { //Maps the criteria json to a grid with title and images
                     return( 
                     
                     <Grid item xs = {12} sm = {6} lg = {4}
-                     marginBottom = {"4%"}
-                     marginTop ={"2%"}
+                     marginBottom = {"2%"}
                      overflow = "hidden"//Resizes the grid based on screen size
                      key = {criteria.title}
                      > 
 
-                    <Box width = "75%">
+                    <Box width = "75%" marginLeft = {"12%"}>
                         <div align={"left"}>
-                        <img src = {criteria.image} width = {"30%"} height = {"30%"} marginbottom = {"10%"}/>
+                        <img src = {criteria.image} width = {"30%"} 
+                        height = {"30%"} />
 
                         <Typography  //Sets h1 for he titles and h2 for the text contents
-                        component = {"h1"} 
+                        component = {"h2"} 
                         fontSize = {"24px"} 
                         fontWeight ={"bold"}
-                        marginLeft = {"2%"}
                         marginTop = {"5%"}>
                             {criteria.title}
                         </Typography>
                         </div>
                         <Typography 
-                        component = {"h2"}
-                        fontFamily={"Roboto"} 
+                        component = {"h3"}
                         fontSize = {"24px"}  
-                        fontWeight= {400} 
                         align = {"left"}
-                        color={"#0B2538"}
+                        color={"primary.dark"}
                         >
                             {criteria.body}
 
