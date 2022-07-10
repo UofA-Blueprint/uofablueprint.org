796a6739a508699eec92b4553777e02f32f19771
Aaron383 <aaronboyd385@gmail.com>
2022-07-09 20:09:04 -0600

    made it look more like mockups and cleaned up code

--- frontend/src/components/nonprofitApply/Service.js
+++ frontend/src/components/nonprofitApply/Service.js
@@ -7,47 +7,41 @@ function Service() {
 
         <Box sx={{
             bgcolor: 'secondary.light',
-
         }}>
           
-          
-        <Typography variant='h4' align="right" style={{ fontWeight: 'bold' }} padding={4}>
-            Our Services             
-        </Typography>
+        <Box sx={{
+            mr: '25%'
+        }}>
+            <Typography variant='h4' align="right" style={{ fontWeight: 'bold' }} paddingTop={4}>
+                Our Services             
+            </Typography>
+        </Box>
        
-
           
         <Box sx={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-            ml: '10%',
-            mr: '10%',
-            
-    
-            
+            ml: '30%',
+            mr: '30%',
         }}>
 
-
-               <Box component="img" marginRight={5} marginBottom={5}
-                    sx={{ height: 125, width: 125 }}
-                    src="/assets/monitor.png" />
-
+            <Box component="img" marginRight={5} marginBottom={5}
+                sx={{ height: 125, width: 125 }}
+                src="/assets/monitor.png" />
 
 
-               <Box marginBottom={5}>
-                    <Typography color='primary' fontWeight={'bold'} fontSize={20} marginBottom={1}>Web Development</Typography>
+            <Box marginBottom={5}>
+                <Typography color='primary' fontWeight={'bold'} fontSize={27} marginBottom={1}>Web Development</Typography>
 
-                    <Typography color='secondary' >Web applications let your users access information from any browser.
-                    This might be better for applications that need to be publicly available or ones that help facilitate task management.</Typography>
+                <Typography color='secondary' >Web applications let your users access information from any browser.
+                This might be better for applications that need to be publicly available or ones that help facilitate task management.</Typography>
+            </Box>
                
-               </Box>
-               
-          </Box>
-          </Box>
+        </Box>
+        </Box>
           
     </div>
-
 }
 
 export default Service
