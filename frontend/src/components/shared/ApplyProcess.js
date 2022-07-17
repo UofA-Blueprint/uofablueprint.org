ab89ae89a40ed5cd235c425af8a2a0c653658057
Aaron383 <aaronboyd385@gmail.com>
2022-07-17 17:24:30 -0600

    made component take data from another file

--- frontend/src/components/shared/ApplyProcess.js
+++ frontend/src/components/shared/ApplyProcess.js
@@ -1,7 +1,62 @@
 import React from 'react'
+import ApplyProcess from '../../../src/data/nonprofitApplyProcess.json'
+import { Typography, Box } from '@mui/material'
 
-function ApplyProcess() {
-    return <div>-generic apply process component placeholder-</div>
+
+function ApplicationProcess() {
+    
+    
+    return <div>
+
+
+        {ApplyProcess.map(applyProcess => {
+            return (
+                   
+                <div key = {applyProcess.number}>
+
+                
+                    <Box sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        ml: '30%',
+                        mr: '30%',
+                    }}>
+                        
+                        <Box>
+                            <Typography marginRight={5} fontSize={90} color={'primary'} fontWeight={500}>
+                                {applyProcess.number}
+                            </Typography>
+                        </Box>
+
+                        <Box>
+                            <Typography color='primary' fontWeight={'bold'} fontSize={27} marginBottom={1}>
+                                {applyProcess.title}
+                            </Typography>
+     
+                            <Typography color='secondary'>
+                                {applyProcess.body}
+                            </Typography>
+                        </Box>   
+
+                    </Box>
+
+
+                <Box sx={{
+                    ml: '32.5%',
+                }}>
+                    <Typography fontSize={50} color={'primary'} fontWeight={'bold'}>
+                        {applyProcess.divider}
+                    </Typography>
+                </Box>
+
+        </div>
+
+                )
+        })}
+
+    </div>
+
 }
 
-export default ApplyProcess
+export default ApplicationProcess
