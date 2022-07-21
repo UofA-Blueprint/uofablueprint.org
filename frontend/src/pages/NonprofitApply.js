6864134eb5dba0c089dffb20bf578648c7732f5b
Aaron <aaronboyd385@gmail.com>
2022-07-20 20:47:18 -0600

    changes the the page

--- frontend/src/pages/NonprofitApply.js
+++ frontend/src/pages/NonprofitApply.js
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import ApplyProcessData from '../data/nonprofitApplyProcess.json'
 import { Banner, ApplyProcess, Faq } from '../components/shared'
-import { Service, Criteria } from '../components/nonprofitApply'
+import { Service, Criteria, ApplicationProcess } from '../components/nonprofitApply'
 
 function NonprofitApply() {
     return (
         <div>
             <Banner />
             <Service />
-            <ApplyProcess />
+            <ApplicationProcess />
+            <ApplyProcess ApplyProcessData={ApplyProcessData}/>
             <Criteria />
             <Faq />
         </div>
