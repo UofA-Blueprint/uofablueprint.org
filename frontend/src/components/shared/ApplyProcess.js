46fcebf8a86644e73956f081d6d1c8892336a174
Dimas Sudjito <almuhtad@ualberta.ca>
2022-07-24 15:16:57 -0600

    fix proptype issue, refactor ApplyProcess.js to include title

--- frontend/src/components/shared/ApplyProcess.js
+++ frontend/src/components/shared/ApplyProcess.js
@@ -1,61 +1,80 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Typography, Box } from '@mui/material'
 
+function ApplicationProcess({ ApplyProcessData, title }) {
+    return (
+        <div>
+            <Typography
+                variant="h4"
+                align="center"
+                style={{ fontWeight: 'bold' }}
+                padding={5}
+            >
+                {title}
+            </Typography>
+            {ApplyProcessData.map((applyProcess) => {
+                return (
+                    <div key={applyProcess.number}>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                                ml: '30%',
+                                mr: '30%',
+                            }}
+                        >
+                            <Box>
+                                <Typography
+                                    marginRight={5}
+                                    fontSize={90}
+                                    color={'primary'}
+                                    fontWeight={500}
+                                >
+                                    {applyProcess.number}
+                                </Typography>
+                            </Box>
 
-function ApplicationProcess({ApplyProcessData}) {
-    
-    
-    return <div>
+                            <Box>
+                                <Typography
+                                    color="primary"
+                                    fontWeight={'bold'}
+                                    fontSize={27}
+                                    marginBottom={1}
+                                >
+                                    {applyProcess.title}
+                                </Typography>
 
-
-        {ApplyProcessData.map(applyProcess => {
-            return (
-                   
-                <div key = {applyProcess.number}>
-
-                
-                    <Box sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'space-between',
-                        ml: '30%',
-                        mr: '30%',
-                    }}>
-                        
-                        <Box>
-                            <Typography marginRight={5} fontSize={90} color={'primary'} fontWeight={500}>
-                                {applyProcess.number}
-                            </Typography>
+                                <Typography color="secondary">
+                                    {applyProcess.body}
+                                </Typography>
+                            </Box>
                         </Box>
 
-                        <Box>
-                            <Typography color='primary' fontWeight={'bold'} fontSize={27} marginBottom={1}>
-                                {applyProcess.title}
+                        <Box
+                            sx={{
+                                ml: '32.5%',
+                            }}
+                        >
+                            <Typography
+                                fontSize={50}
+                                color={'primary'}
+                                fontWeight={'bold'}
+                            >
+                                {applyProcess.divider}
                             </Typography>
-     
-                            <Typography color='secondary'>
-                                {applyProcess.body}
-                            </Typography>
-                        </Box>   
-
-                    </Box>
-
-
-                <Box sx={{
-                    ml: '32.5%',
-                }}>
-                    <Typography fontSize={50} color={'primary'} fontWeight={'bold'}>
-                        {applyProcess.divider}
-                    </Typography>
-                </Box>
-
-        </div>
-
+                        </Box>
+                    </div>
                 )
-        })}
+            })}
+        </div>
+    )
+}
 
-    </div>
-
+ApplicationProcess.propTypes = {
+    ApplyProcessData: PropTypes.array,
+    title: PropTypes.string,
 }
 
 export default ApplicationProcess
--- frontend/src/pages/NonprofitApply.js
+++ frontend/src/pages/NonprofitApply.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import ApplyProcessData from '../data/nonprofitApplyProcess.json'
 import { Banner, ApplyProcess, Faq } from '../components/shared'
-import { Service, Criteria, ApplicationProcess } from '../components/nonprofitApply'
+import { Service, Criteria } from '../components/nonprofitApply'
 
 function NonprofitApply() {
     return (
         <div>
             <Banner />
             <Service />
-            <ApplicationProcess />
-            <ApplyProcess ApplyProcessData={ApplyProcessData}/>
+            <ApplyProcess
+                ApplyProcessData={ApplyProcessData}
+                title="Nonprofits Application Process"
+            />
             <Criteria />
             <Faq />
         </div>
