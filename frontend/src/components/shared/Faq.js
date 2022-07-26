3cfa4d1f61f6347f932624dbe12b3dfd80f6e92e
Aaron <aaronboyd385@gmail.com>
2022-07-25 21:08:41 -0600

    faq with json data array

--- frontend/src/components/shared/Faq.js
+++ frontend/src/components/shared/Faq.js
@@ -1,19 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Box, Typography } from '@mui/material';
 
 
-function Faq(props) {
+function Faq({FaqData}) {
     return (
-        <Box sx={{
+        <Box padding={3} sx={{
             display: 'flex',
-            flexDirection: 'row',
+            flexDirection: 'column',
             alignItems: 'center',
             justifyContent: 'center',
         }} bgcolor="primary.dark">
-            <Typography fontSize={20} color='primary.light'> Frequently Asked Questions</Typography>
-            <h1> hello, {props.name} </h1>
+
+            <Typography fontSize={40} color='primary.light'> Frequently Asked Questions</Typography>
+
+            {FaqData.map((Faq) => {
+                return (
+                    <Box key={Faq.title} padding={4} sx={{
+                        mr: '18%',
+                        ml: '18%'
+                    }}>
+                        
+                        <Typography color={'white'} fontSize={30} marginBottom={1}>{Faq.title}</Typography>
+                        <Typography color={'white'} fontSize={18}>{Faq.body}</Typography>
+
+                    </Box>
+                )
+            })}
+
         </Box>
     )
 }
+
+Faq.propTypes = {
+    FaqData: PropTypes.array,
+}
+
 
 export default Faq
