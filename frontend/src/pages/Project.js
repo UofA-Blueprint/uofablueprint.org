aabe03d71de3684671cc849e49b14266e6dc6625
Gurveer Sohal <gurveersohal433@gmail.com>
2022-07-27 20:20:15 -0600

    styled project page

--- frontend/src/pages/Project.js
+++ frontend/src/pages/Project.js
@@ -2,12 +2,21 @@ import React from 'react'
 
 import { Banner, ProjectList } from '../components/shared'
 
+let projectsObj = require('../../src/data/projects.json')
+
 function Project() {
     return (
         <div id="projects">
-            <Banner />
-            <ProjectList title="Current Projects" />
-            <ProjectList title="Past Projects" />
+            <Banner 
+                header="Our Projects"
+                body="All of Blueprint's work is open source because we believe in building technology that makes us more open and connected."
+                buttonText="View our github"
+                externalLink="https://github.com/UofA-Blueprint"
+            />
+
+            <ProjectList title="Current Projects" projects={projectsObj.current}/>
+
+            <ProjectList title="Past Projects" projects={projectsObj.past}/>
         </div>
     )
 }
