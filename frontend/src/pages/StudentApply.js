b169cef3ffb01512044229dfbb0ffe082c14f485
Parblack <jfernan1@ualberta.ca>
2022-07-30 12:49:48 -0600

    Fixed some margins, fixed gray background and added student app process stuff, banner has been added aswell

--- frontend/src/pages/StudentApply.js
+++ frontend/src/pages/StudentApply.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import ApplyProcessData from '../data/nonprofitApplyProcess.json'
+import ApplyProcessData from '../data/studentApplyProcess.json'
 import { Banner, ApplyProcess, Faq } from '../components/shared'
 import { Criteria } from '../components/studentApply'
 import FaqData from '../data/NonprofitFaqData.json' // replace with student FAQ mock data
@@ -8,11 +8,20 @@ import FaqData from '../data/NonprofitFaqData.json' // replace with student FAQ 
 function StudentApply() {
     return (
         <div>
-            <Banner />
-            <ApplyProcess
-                ApplyProcessData={ApplyProcessData}
-                title="Students Application Process"
-            />
+            <Banner
+                header='For Students' 
+                body='Joining Blueprint is a great way to give back to your community. Help us build our applications and work with nonprofits!'
+                buttonText='Join our team'
+                buttonLink="/apply/students#studentApplyProcess">
+
+            </Banner>
+            <div id = "studentApplyProcess"
+            style={{marginBottom: "3%"}}>
+                <ApplyProcess
+                    ApplyProcessData={ApplyProcessData} //Div is so the hashlink button can scroll down
+                    title="Students Application Process"
+                />
+            </div>
             <Criteria />
             <Faq FaqData={FaqData}/>
         </div>
