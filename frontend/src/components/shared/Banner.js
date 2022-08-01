5aa5eca31e8208bd57eefb26ec4a8de0702b4dce
Dimas Sudjito <almuhtad@ualberta.ca>
2022-08-01 11:59:50 -0600

    replace mock data for student apply faq

--- frontend/src/components/shared/Banner.js
+++ frontend/src/components/shared/Banner.js
@@ -3,47 +3,58 @@ import PropTypes from 'prop-types'
 import { Box } from '@mui/system'
 import React from 'react'
 import { Theme } from '../../config/Theme'
-import {HashLink} from 'react-router-hash-link'
+import { HashLink } from 'react-router-hash-link'
 
 function Banner({ header, body, buttonText, buttonLink }) {
     return (
-        <Box sx={{
-            background: Theme.palette.primary.dark,
-            color: Theme.palette.primary.light,
-            height: 300
-        }}>
-            <Typography variant='h4' sx={{
-                p: 2,
-                pl: 4,
-                fontWeight: 'bold'
-            }}>
+        <Box
+            sx={{
+                background: Theme.palette.primary.dark,
+                color: Theme.palette.primary.light,
+                height: 300,
+            }}
+        >
+            <Typography
+                variant="h4"
+                sx={{
+                    p: 2,
+                    pl: 4,
+                    fontWeight: 'bold',
+                }}
+            >
                 {header}
             </Typography>
 
-            <Typography variant='body1' sx={{
-                p: 2,
-                pl: 4,
-                fontSize: 20,
-                fontWeight: 'medium',
-                width: 500
-            }}>
+            <Typography
+                variant="body1"
+                sx={{
+                    p: 2,
+                    pl: 4,
+                    fontSize: 20,
+                    fontWeight: 'medium',
+                    width: 500,
+                }}
+            >
                 {body}
             </Typography>
 
-            <HashLink to={buttonLink} smooth> 
-                <Button variant='outlined' size='small' sx={{
-                    m: 2,
-                    ml: 4,
-                    color: Theme.palette.primary.light,
-                    borderColor: Theme.palette.secondary.light,
-                    "&:hover": {
-                        borderColor: Theme.palette.primary.light
-                    },
-                }}>
+            <HashLink to={buttonLink} smooth style={{ textDecoration: 'none' }}>
+                <Button
+                    variant="outlined"
+                    size="small"
+                    sx={{
+                        m: 2,
+                        ml: 4,
+                        color: Theme.palette.primary.light,
+                        borderColor: Theme.palette.secondary.light,
+                        '&:hover': {
+                            borderColor: Theme.palette.primary.light,
+                        },
+                    }}
+                >
                     {buttonText}
                 </Button>
             </HashLink>
-
         </Box>
     )
 }
@@ -56,11 +67,10 @@ Banner.propTypes = {
 }
 
 Banner.defaultProps = {
-    header: "",
-    body: "",
-    buttonText: "",
-    buttonLink: "",
+    header: '',
+    body: '',
+    buttonText: '',
+    buttonLink: '',
 }
-
 
 export default Banner
