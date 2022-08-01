a66aba3cb399c9b432ec9759168b3ecb20e2d579
Dimas Sudjito <almuhtad@ualberta.ca>
2022-08-01 11:49:23 -0600

    make project list display conditionals

--- frontend/src/pages/Project.js
+++ frontend/src/pages/Project.js
@@ -7,16 +7,24 @@ let projectsObj = require('../../src/data/projects.json')
 function Project() {
     return (
         <div id="projects">
-            <Banner 
+            <Banner
                 header="Our Projects"
                 body="All of Blueprint's work is open source because we believe in building technology that makes us more open and connected."
                 buttonText="View our github"
                 externalLink="https://github.com/UofA-Blueprint"
             />
-
-            <ProjectList title="Current Projects" projects={projectsObj.current}/>
-
-            <ProjectList title="Past Projects" projects={projectsObj.past}/>
+            {projectsObj.current.length == 0 ? null : (
+                <ProjectList
+                    title="Current Projects"
+                    projects={projectsObj.current}
+                />
+            )}
+            {projectsObj.past.length == 0 ? null : (
+                <ProjectList
+                    title="Past Projects"
+                    projects={projectsObj.past}
+                />
+            )}
         </div>
     )
 }
